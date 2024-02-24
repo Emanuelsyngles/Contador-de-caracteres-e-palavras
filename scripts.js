9151a3c4d4ac863f6fc4c43b9e1a77b69aec2602
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-02-24 20:15:43 -0300

    Script e daptação de tela finalizados.

--- scripts.js
+++ scripts.js
@@ -4,29 +4,31 @@ const toggleButton = document.querySelector("#toggle");
 
 let mode = "characters"
 
-toggleButton.addEventListener('click', () => {
+toggleButton.addEventListener("click", () => {
 
     if(mode === "characters") {
-        mode = "words";
+        mode = "words"
         toggleButton.textContent = "Contar caracteres";
-    } else {
-        mode = "characters"
-        toggleButton.textContent = "Contar palavras"
+    }else {
+        mode = "characters";
+        toggleButton.textContent = "Contar palavras";
     }
 
-})
+    input.dispatchEvent(new Event("input"))
+});
 
 input.addEventListener("input", () => {
-
     let count = 0;
 
-    if(mode === "characters") {
-        count = input.value.length
+    if (mode === "characters") {
+        count = input.value.length;
 
         counter.textContent = `${count} caractere(s)`
-        
-    }else {
-        
+    } else {
+        const words = input.value.trim().split(/\s+/)
+        count = input.value.trim() === "" ? 0 : words.length;
+
+        counter.textContent = `${count} palavras`
     }
 
 })
